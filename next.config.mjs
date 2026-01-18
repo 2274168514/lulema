070c8054d78af3bf7e3e739eb4104ba0e490dbaf
2274168514 <2274168514@qq.com>
2026-01-19 00:27:35 +0800

    config: setup Cloudflare Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
@@ -15,5 +17,9 @@ const nextConfig = {
     },
   },
 };
+
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
